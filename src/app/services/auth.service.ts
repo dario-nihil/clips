70936fa53fadd704483a9fbbdc64bc86f45e7ff1
dario-nihil <dario.nihil@gmail.com>
2022-09-13 15:59:38 +0200

    improved login experience

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,7 +5,7 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
 } from '@angular/fire/compat/firestore';
-import { map, Observable } from 'rxjs';
+import { delay, map, Observable } from 'rxjs';
 import IUser from '../models/user.model';
 
 @Injectable({
@@ -14,10 +14,12 @@ import IUser from '../models/user.model';
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>;
   public isAuthenticated$: Observable<boolean>;
+  public isAuthenticatedWithDelay$: Observable<boolean>;
 
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
     this.usersCollection = this.db.collection('users');
     this.isAuthenticated$ = this.auth.user.pipe(map((user) => !!user));
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
   }
 
   public async createUser(userData: IUser) {
