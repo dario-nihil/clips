218a23a1d275cb16e4733ad585c6e258ce67d471
dario-nihil <dario.nihil@gmail.com>
2022-09-14 16:56:36 +0200

    refined logout behavior by adding data to the router

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,9 +5,10 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
 } from '@angular/fire/compat/firestore';
-import { delay, map, Observable } from 'rxjs';
+import { delay, filter, map, Observable, of, switchMap } from 'rxjs';
 import IUser from '../models/user.model';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -16,15 +17,24 @@ export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>;
   public isAuthenticated$: Observable<boolean>;
   public isAuthenticatedWithDelay$: Observable<boolean>;
+  private redirect = false;
 
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFirestore,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     this.usersCollection = this.db.collection('users');
     this.isAuthenticated$ = this.auth.user.pipe(map((user) => !!user));
     this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
+    this.router.events
+      .pipe(
+        filter((e) => e instanceof NavigationEnd),
+        map((e) => this.route.firstChild),
+        switchMap((route) => route?.data ?? of({}))
+      )
+      .subscribe((data) => (this.redirect = data?.['authOnly'] ?? false));
   }
 
   public async createUser(userData: IUser) {
@@ -63,6 +73,8 @@ export class AuthService {
 
     await this.auth.signOut();
 
-    await this.router.navigateByUrl('/');
+    if (this.redirect) {
+      await this.router.navigateByUrl('/');
+    }
   }
 }
