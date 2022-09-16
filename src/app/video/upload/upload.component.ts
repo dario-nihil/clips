bc91383de8879bc0941ca542e8d8ae11a5160c0a
dario-nihil <dario.nihil@gmail.com>
2022-09-16 12:55:59 +0200

    implementing video file upload

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+  styleUrls: ['./upload.component.css'],
 })
 export class UploadComponent implements OnInit {
+  isDragOver = false;
+  file: File | null = null;
+  nextStep = false;
 
-  constructor() { }
+  title = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(3)],
+    nonNullable: true,
+  });
 
-  ngOnInit(): void {
+  uploadForm = new FormGroup({
+    title: this.title,
+  });
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  storeFile($event: Event) {
+    this.isDragOver = false;
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return;
+    }
+
+    this.nextStep = true;
+    this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
+
+    console.log(this.file);
   }
 
+  uploadFile() {
+    console.log('inside onSubmit');
+    console.log(this.uploadForm.value);
+  }
 }
